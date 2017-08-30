c840bb6ae37638abf15e09c97cbf823b1ef9e4a3
ashishsingh4086 <ashish.singh4086@outlook.com>
2017-08-30 17:37:11 -0400

    added billing and client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import {BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const App=()=>{
-    return(
-        <div>
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+import Header from "./Header";
+import Landing from "./Landing";
+const DashBoard = () => <h2> Dashboard </h2>;
+const SurveyNew = () => <h2> SurveyNew</h2>;
 
-            Hi there ! 
-            </div>
+class App extends Component {
+   componentDidMount(){
+      this.props.fetchUser();
+   }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact={true} path="/" component={Landing} />
+            <Route exact={true} path="/surveys" component={DashBoard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
-};
-
-export default App;
+  }
+}
+export default connect(null,actions)(App);
