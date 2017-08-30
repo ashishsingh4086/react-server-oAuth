a154be3846b483a2f7c951b042fbcc33d13c1f82
ashishsingh4086 <ashish.singh4086@outlook.com>
2017-08-30 17:40:23 -0400

    new add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const keys = require("./config/key");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 
 require("./models/User");
 require("./services/passport");
@@ -13,6 +14,7 @@ mongoose.connect(`${keys.mongoURI}`);
 console.log(mongoose.connection.readyState);
 
 const app = express();
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,6 +25,15 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 authRoutes(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
